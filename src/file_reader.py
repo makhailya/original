230cac412fcd9a45e39d919f5c9ba2e1da86cec8
makhailya <makhailya@gmail.com>
2025-09-26 18:18:42 +0300

    flake8 fixes

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,16 +1,13 @@
-# src/file_reader.py
 from __future__ import annotations
-
-from pathlib import Path
-from typing import Any, Dict, List
-
+from typing import Any, List, Dict
 import pandas as pd
-from typing import Any
 
-def read_transactions_csv(path: str) -> list[dict[str, Any]]:
+
+def read_transactions_csv(path: str) -> List[Dict[str, Any]]:
     df = pd.read_csv(path)
     return df.to_dict(orient="records")  # type: ignore
 
-def read_transactions_excel(path: str) -> list[dict[str, Any]]:
+
+def read_transactions_excel(path: str) -> List[Dict[str, Any]]:
     df = pd.read_excel(path)
     return df.to_dict(orient="records")  # type: ignore
